9e5f72ff967e19b137f6fe725ce311e5e55dee33
ikken <pierosaucedo3@gmail.com>
2024-08-20 01:50:20 -0300

    Done

--- src/graph.rs
+++ src/graph.rs
@@ -34,15 +34,17 @@ impl Graph {
                 continue;
             }
 
-            println!("heap {:?}", visited);
+            println!("Visited {:?}", visited);
 
-            for neighbor in &self.vertices.get(&current_vertex).unwrap().edges {
-                if let Some(next) = self.vertices.get(&neighbor.to) {
-                    let distance = current_distance + neighbor.weight;
-                    if distance < *distances.get(&neighbor.to).unwrap_or(&u32::MAX) {
-                        distances.insert(neighbor.to.clone(), distance);
+            if let Some(v) = self.vertices.get(&current_vertex) {
+                for neighbor in &v.edges {
+                    if let Some(next) = self.vertices.get(&neighbor.to) {
+                        let distance = current_distance + neighbor.weight;
 
-                        priority_queue.push(State { vertex: next.id, cost: distance });
+                        if distance < *distances.get(&neighbor.to).unwrap_or(&u32::MAX) {
+                            distances.insert(neighbor.to.clone(), distance);
+                            priority_queue.push(State { vertex: next.id, cost: distance });
+                        }
                     }
                 }
             }
@@ -52,27 +54,31 @@ impl Graph {
             println!("\nFrom vertex \'{}\' to vertex \'{}\':", start.id, to);
             println!("  Cost: {}", cost);
         }
+
+        print!("Visited len {}", visited.len())
     }
 
     pub fn dijkstra_no_heap(&mut self, start: Vertex) {
         let mut distances: HashMap<VertexId, u32> = HashMap::new();
         let mut visited: HashSet<VertexId> = HashSet::new();
 
-        distances.insert(start.id, 0);
+        distances.insert(start.id.clone(), 0);
 
-        let mut current_vertex = start.clone();
-        let graph_size = self.vertices.keys().len();
+        let mut current_vertex = start.id.clone();
+        let graph_len = self.vertices.keys().len();
 
-        while visited.len() < graph_size {
-            visited.insert(current_vertex.id);
-            println!("no heap {:?}", visited);
-            let current_distance = *distances.get(&current_vertex.id).unwrap_or(&u32::MAX);
+        while visited.len() < graph_len {
+            visited.insert(current_vertex);
+            println!("Visited {:?}", visited);
+            let current_distance = *distances.get(&current_vertex).unwrap_or(&u32::MAX);
 
-            for neighbor in &current_vertex.edges {
-                let distance = current_distance + neighbor.weight;
+            if let Some(v) = self.vertices.get(&current_vertex) {
+                for neighbor in &v.edges {
+                    let distance = current_distance + neighbor.weight;
 
-                if distance < *distances.get(&neighbor.to).unwrap_or(&u32::MAX) {
-                    distances.insert(neighbor.to, distance);
+                    if distance < *distances.get(&neighbor.to).unwrap_or(&u32::MAX) {
+                        distances.insert(neighbor.to, distance);
+                    }
                 }
             }
 
@@ -83,7 +89,7 @@ impl Graph {
                 .map(|(_, v)| v.clone());
 
             match next_vertex {
-                Some(v) => current_vertex = v,
+                Some(v) => current_vertex = v.id,
                 None => break,
             }
         }
@@ -92,6 +98,8 @@ impl Graph {
             println!("\nFrom vertex \'{}\' to vertex \'{}\':", start.id, to);
             println!("  Cost: {}", cost);
         }
+
+        print!("Visited len {}", visited.len())
     }
 }
 
